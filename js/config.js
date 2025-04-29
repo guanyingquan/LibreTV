a73ac16c6c99ffe56c94f8b2524118a3b84b9954
guanyingquan <guanyingquan227@gmail.com>
2025-04-30 03:25:17 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -40,7 +40,7 @@ const API_SITES = {
     ckzy: {
         api: 'https://www.ckzy1.com',
         name: 'CK资源',
-        adult: true
+        // adult: true
     },
     zy360: {
         api: 'https://360zy.com',
@@ -86,32 +86,32 @@ const API_SITES = {
     jkun: {
         api: 'https://jkunzyapi.com',
         name: 'jkun资源',
-        adult: true
+        // adult: true
     },
     bwzy: {
         api: 'https://api.bwzym3u8.com',
         name: '百万资源',
-        adult: true
+        // adult: true
     },
     souav: {
         api: 'https://api.souavzy.vip',
         name: 'souav资源',
-        adult: true
+        // adult: true
     },
     r155: {
         api: 'https://155api.com',
         name: '155资源',
-        adult: true
+        // adult: true
     },
     lsb: {
         api: 'https://apilsbzy1.com',
         name: 'lsb资源',
-        adult: true
+        // adult: true
     },
     huangcang: {
         api: 'https://hsckzy.vip',
         name: '黄色仓库',
-        adult: true,
+        // adult: true,
         detail: 'https://hsckzy.vip'
     },
     zuid: {
@@ -121,7 +121,7 @@ const API_SITES = {
     yutu: {
         api: 'https://yutuzy10.com',
         name: '玉兔资源',
-        adult: true
+        // adult: true
     }
     // 您可以按需添加更多源
 };
